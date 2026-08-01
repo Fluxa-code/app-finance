d864905559a8bf25cf543b908ebb9f1b826727ad
deivid127 <deivid123.leite@gmail.com>
2026-08-01 14:53:47 -0300

    chore: projeto Spring Boot inicial com Postgres via Docker Compose

---
+++ src/test/java/br/com/deivid/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deivid.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
